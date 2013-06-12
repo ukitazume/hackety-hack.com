41618a968b9ca55f77b083c4839fb715e77fcca1
Yu Kitazume <u.kitazume@gmail.com>
2013-06-12 16:25:25 +0900

    for engine yard

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'jquery-rails'
 gem 'mongo_mapper'
 gem 'bson_ext'
 
+gem 'mongo'
+gem 'ey_config'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -4,6 +4,8 @@ if ENV['MONGOHQ_URL']
   }
 
   MongoMapper.connect(Rails.env)
+elsif Rails.env == 'production'
+  MongoMapper.config = {'production' => {'uri' => URI.parse(EY::Config.get(:mongolab, 'MONGOLAB_URI'))}}
 else
   MongoMapper.database = "hackety-hack-com-#{Rails.env}"
 end
